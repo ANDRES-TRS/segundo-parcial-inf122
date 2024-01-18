ef6c6e73c48196f9add9b98d43cf63b020db7256
andresEL100xd <118159274+andresEL100xd@users.noreply.github.com>
2024-01-18 11:13:20 -0400

    readme

---
+++ public/script.js
@@ -0,0 +1,9 @@
+function agregarMeta() {
+    var nuevaMeta = document.getElementById("nuevaMeta").value;
+
+    console.log("Nueva meta: " + nuevaMeta);
+
+    // Aquí puedes agregar lógica adicional, como enviar el dato a un servidor, etc.
+
+    document.getElementById("formularioMetas").reset();
+}
